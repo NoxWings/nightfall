f3c9db1c55cac3e6658fe17c96b86d1574734d4f
NoxWings <david@gamevy.com>
2018-01-28 15:16:17 +0100

    Add chai to test globals

--- test/helpers/helper.js
+++ test/helpers/helper.js
@@ -4,6 +4,6 @@ var chai = require("chai");
 chai.use(require("sinon-chai"));
 chai.use(require("chai-as-promised"));
 
+global.chai = chai;
 global.expect = chai.expect;
 global.assert = chai.assert;
-
