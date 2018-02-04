8d0362571922a2e6136460c7d0c9cb47227846d6
David Garcia Miguel <noxwings@gmail.com>
2018-02-04 13:52:16 +0100

    Ocean tweaks

--- src/components/main_scene/subviews/create_ocean.js
+++ src/components/main_scene/subviews/create_ocean.js
@@ -1,14 +1,15 @@
 import BABYLON from "babylonjs";
 
 export default function (scene) {
-    var waterMesh = BABYLON.Mesh.CreateGround("Ocean", 2048, 2048, 32, scene, false);
+    var waterMesh = BABYLON.Mesh.CreateGround("Ocean", 2048*2, 2048*2, 32*2, scene, false);
     var water = new BABYLON.WaterMaterial("water", scene, new BABYLON.Vector2(512, 512));
+    window.water = water;
     water.backFaceCulling = true;
     water.bumpTexture = new BABYLON.Texture("assets/waterbump.png", scene);
-    water.windForce = 3;
+    water.windForce = 2.5;
     water.waveSpeed = 30;
-    water.waveHeight = 1;
-    water.bumpHeight = 0.2;
+    water.waveHeight = 0.5;
+    water.bumpHeight = 0.3;
     water.windDirection = new BABYLON.Vector2(1, 1);
     water.waterColor = new BABYLON.Color3(0, 0, 221 / 255);
     water.colorBlendFactor = 0.002;
