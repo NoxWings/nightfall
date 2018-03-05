973928bf40ef78919a18e8147590bbf1a2556e20
NoxWings <david@gamevy.com>
2018-03-05 22:26:40 +0100

    Add a little bit more variation

--- src/components/main_scene/actions/camera_movement.js
+++ src/components/main_scene/actions/camera_movement.js
@@ -2,18 +2,27 @@ import { Tools } from "babylonjs";
 
 
 export default function createCameraMovement(camera, {
-    yVariation = 5,
+    yPosVariation = 5,
     yCicles = 0.3,
     rollVarition = 4,
-    rollCicles = 0.4
+    rollCicles = 0.4,
+    yawVariation = 25,
+    yawCicles = 0.01
 } = {}) {
     const yPos = camera.position.y;
+    const yRot = camera.rotation.y;
     const zRot = camera.rotation.z;
+
+    const yPosRange = yPosVariation / 2;
+    const yRotRange = Tools.ToRadians(yawVariation / 2);
+    const zRotRange = Tools.ToRadians(rollVarition / 2);
 
     return () => {
         var now = performance.now();
+        var angularVariation = Math.PI * now / 1000;
 
-        camera.position.y = yPos + yVariation/2 * Math.sin(yCicles * Math.PI * now / 1000);
-        camera.rotation.z = zRot + Tools.ToRadians(rollVarition/2) * Math.sin(rollCicles * Math.PI * now / 1000);
+        camera.position.y = yPos + yPosRange * Math.sin(yCicles * angularVariation);
+        camera.rotation.y = yRot + yRotRange * Math.sin(yawCicles * angularVariation);
+        camera.rotation.z = zRot + zRotRange * Math.sin(rollCicles * angularVariation);
     };
 }
