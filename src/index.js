25b72172b0a9be94ba8c38bcec7800e65c806e22
NoxWings <david@gamevy.com>
2018-03-15 23:38:23 +0100

    Remove manual camera control

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ export default function app () {
     const engine = new BABYLON.Engine(canvas, true);
 
     let scene = new MainScene(engine);
-    scene.mainCamera.attachControl(canvas, false);
 
     engine.runRenderLoop(function () {
         scene.render();
