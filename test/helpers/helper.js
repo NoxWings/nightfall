2e633dbe5a7041f98bfb1b8c05924554d75de641
David (NoxWings) García Miguel <noxwings@gmail.com>
2019-04-27 11:49:43 +0200

    Remove test folder as it is not used

--- test/helpers/helper.js
+++
@@ -1,9 +0,0 @@
-global.sinon = require("sinon");
-
-const chai = require("chai");
-chai.use(require("sinon-chai"));
-chai.use(require("chai-as-promised"));
-
-global.chai = chai;
-global.expect = chai.expect;
-global.assert = chai.assert;
